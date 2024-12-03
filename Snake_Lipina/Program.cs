bca0afeea7e93463d86e3629571fa4067168754b
klipina53 <klipina53@gmail.com>
2024-12-03 16:39:22 +0500

    Добавленные переменные сервера.Липина

--- Snake_Lipina/Program.cs
+++ Snake_Lipina/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,12 @@ namespace Snake_Lipina
 {
     internal class Program
     {
+        public static List<Leaders> Leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        private static readonly int localPort = 5001;
+        public static int MaxSpeed = 15;
+
         static void Main(string[] args) 
         {
         }
